8564cf3663bd4bb675194b43f8434690cc7999bf
ZeroJ <zeroj@jasnig.local>
2016-07-04 22:15:10 +0800

    Initial Commit

---
+++ CameraView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CameraView
+//
+//  Created by ZeroJ on 16/7/4.
+//  Copyright © 2016年 ZeroJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
